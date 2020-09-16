7dbd54f7c936eb279b00f824083ed2770108a1b7
Lena Platonova <platon@gmail.com>
2020-09-16 10:57:13 +0900

    2 commet

---
+++ 1.py
@@ -0,0 +1,11 @@
+#зад 21
+x=int(input())
+y=int(input())
+import math
+c=math.sqrt(x**2+y**2)
+print(c, 'гипотенуза')
+s=1/2*x*y
+print(s, 'площадь')
+p=x+y+c
+print(p, 'периметр')
+print("first commenttt")
